8de03773437acf99f6428c2ef7ed2c602920d35f
Rui <iamliurui@gmail.com>
2016-11-02 01:46:23 -0400

    First pass add contact section.

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   resources :projects
   resources :posts
+
   # resources :posts do
   #   resources :comments
   # end
 
   root 'welcome#index'
   get 'welcome/index'
+  get 'contact', to: 'contact#index'
 
   get '*path' => redirect('/')
 end
